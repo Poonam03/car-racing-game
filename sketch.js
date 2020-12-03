7ac4b8a76fd81298b37b10ea8c6386f267c8996b
Poonam03 <68641541+Poonam03@users.noreply.github.com>
2020-12-03 09:30:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,12 +14,12 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  track = loadImage("car-racing-game/images/track.jpg");
+  car1_img = loadImage("car-racing-game/images/car1.png");
+  car2_img = loadImage("car-racing-game/images/car2.png");
+  car3_img = loadImage("car-racing-game/images/car3.png");
+  car4_img = loadImage("car-racing-game/images/car4.png");
+  ground = loadImage("car-racing-game/images/ground.png");
 }
 
 function setup(){
